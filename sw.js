fec7eb680780c1b789e03f0942740b4b4eb5f68e
einfachmachen <einfachmachen@users.noreply.github.com>
2026-08-18 11:19:00 +0000

    deploy: 3e8f06ccdf7d4a11e09591329c08a8447de6aada

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// build: 1787051275542
+// build: 1787051931363
 // Service Worker — "network-first" für Seiten-Navigationen, damit IMMER die
 // frische index.html geladen wird (verhindert den alten Ladebildschirm aus dem
 // Cache). Offline gibt es einen Fallback auf die zuletzt gesehene Version.
